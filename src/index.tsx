f46caa4ac1f19c38feda423b9769d4c2e20307fa
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-06-26 20:16:48 +0200

    configured store configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+  combineReducers,
+} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import './index.css';
+import contactsReducer from './store/reducers/contactsReducer';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+});
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancers = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, enhancers);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 const root = ReactDOM.createRoot(
